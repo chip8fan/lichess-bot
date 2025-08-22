ac2f5e2b2973260cc54359c07d352b9af5e2216c
chip8fan <nguyennamtran2022@gmail.com>
2025-08-21 20:38:07 -0500

    Add files via upload

--- rated-bot.py
+++ rated-bot.py
@@ -3,24 +3,21 @@ from dotenv import load_dotenv # load dotenv
 import os # get the environment variable using the os library
 import secrets # random library but more secure
 import sys # to exit the program if it is needed
+import time # to rate-limit requests to the lichess API
 import engine
-import time
 load_dotenv() # load the .env file
 key = os.environ.get("BOT_KEY") # get the BOT_KEY from the .env file (the secure way to store environment variables, NEVER hardcode a key into an app)
 session = berserk.TokenSession(key) # initialize a session
 client = berserk.Client(session=session) # and a client
 for response in client.bots.stream_incoming_events(): # check for challenges and gameStart events
     if response['type'] == "gameStart": # if a gameStart event happens
+        print(response)
         game_id = response['game']['gameId'] # set the game_id
         break # and exit the loop
     elif response['type'] == "challenge": # if a challenge event happens
         print(response) # log the response
         game_id = response['challenge']['id'] # and get the game id
-        if response['challenge']['speed'] == 'ultraBullet': # check if challenge is ultrabullet
-            client.bots.decline_challenge(game_id, "This bot cannot play ultrabullet.") # if it is an ultrabullet challenge reject it
-            sys.exit() # and exit
-        else:
-            client.bots.accept_challenge(game_id)
+        client.bots.accept_challenge(game_id) # accept the challenge
         break # and exit the loop
 for response in client.bots.stream_game_state(game_id): # check for moves
     try: # try to get the moves
@@ -36,7 +33,7 @@ while True: # while bot is running
     try: # try to make a move
         start = time.perf_counter()
         legal_moves = chess_engine.evaluate(all_moves=moves, fen=start_pos, depth=4)
-        move = legal_moves[1][secrets.randbelow(len(legal_moves))] # get a random move
+        move = legal_moves[1][secrets.randbelow(len(legal_moves[1]))] # get a random move
         end = time.perf_counter()-start
         print(legal_moves)
         print(f"{move} made in {end} seconds, eval {legal_moves[0]}")
