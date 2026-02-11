c27198d1e90143752c2da64757480a02aa5a84f0
chip8fan <nguyennamtran2022@gmail.com>
2026-02-11 16:21:34 -0600

    add endgame tb fetch script

---
+++ engines/get_egtb.py
@@ -0,0 +1,18 @@
+import os
+import requests
+import bs4
+if os.path.isdir("syzygy") == False:
+    os.mkdir("syzygy")
+os.chdir("syzygy")
+dtz_links = bs4.BeautifulSoup(requests.get("https://tablebase.lichess.ovh/tables/standard/3-4-5-dtz/").content, 'html.parser').find_all("a")
+for dtz_link in dtz_links:
+    link = str(dtz_link).split('href="')[1].split('"')[0]
+    if link.endswith(".rtbz"):
+        with open(link, "wb") as file:
+            file.write(requests.get(f"https://tablebase.lichess.ovh/tables/standard/3-4-5-dtz/{link}").content)
+wdl_links = bs4.BeautifulSoup(requests.get("https://tablebase.lichess.ovh/tables/standard/3-4-5-wdl/").content, 'html.parser').find_all("a")
+for wdl_link in wdl_links:
+    link = str(wdl_link).split('href="')[1].split('"')[0]
+    if link.endswith(".rtbw"):
+        with open(link, "wb") as file:
+            file.write(requests.get(f"https://tablebase.lichess.ovh/tables/standard/3-4-5-wdl/{link}").content)
