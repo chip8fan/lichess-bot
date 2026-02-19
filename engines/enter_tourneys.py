9743fd173ebb817429a600aea0d919771e99b937
Nguyen Tran <me@mac.local>
2026-02-19 08:56:24 -0600

    allow this command to work python3 lichess-bot.py -u & python3 engines/enter_tourneys.py

--- engines/enter_tourneys.py
+++ engines/enter_tourneys.py
@@ -1,7 +1,7 @@
 import berserk
 import datetime
 import time
-client = berserk.Client(session=berserk.TokenSession([line for line in open("../config.yml")][0].split('"')[1].split('"')[0]), base_url="http://localhost:8080/")
+client = berserk.Client(session=berserk.TokenSession([line for line in open("config.yml")][0].split('"')[1].split('"')[0]), base_url="http://localhost:8080/")
 while True:
     if datetime.datetime.now().hour == 7 and datetime.datetime.now().minute == 0 and datetime.datetime.now().second == 30 or datetime.datetime.now().hour == 19 and datetime.datetime.now().minute == 0 and datetime.datetime.now().second == 30:
         client.tournaments.join_arena(client.tournaments.tournaments_by_user("Human", nb=1)[0]['id'])
