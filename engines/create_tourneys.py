cef4e40d1dc236350fa893e94b931aaa5aa06f89
Nguyen Tran <me@mac.local>
2026-02-26 07:54:52 -0600

    redirect hostname to duckdns site and check every hour for tournaments

--- engines/create_tourneys.py
+++
@@ -1,8 +0,0 @@
-import berserk
-import datetime
-import time
-client = berserk.Client(session=berserk.TokenSession(open("human.txt", "rb").read().decode("utf-8")), base_url="http://localhost:8080/")
-while True:
-    if datetime.datetime.now().hour == 7 and datetime.datetime.now().minute == 0 and datetime.datetime.now().second == 0 or datetime.datetime.now().hour == 19 and datetime.datetime.now().minute == 0 and datetime.datetime.now().second == 0:
-        client.tournaments.create_arena(clockTime=5, clockIncrement=3, minutes=720, rated=True, streakable=True, wait_minutes=60)
-    time.sleep(1)
